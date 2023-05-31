565be0b6d0c16639c1fee3264f3a5d7cf99dbcdc
abhaykumar1321 <abhaykumar132116@gmail.com>
2023-05-31 17:27:20 +0530

    some changes

--- script.js
+++ script.js
@@ -8,11 +8,26 @@ const weather = document.querySelector("#weather")
 const getWeather = async (city) => {
 	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
 	const response = await fetch(url);
+	const data = await response.json()
+	return showWeather(data)
+}
+
+const showWeather = (data) => {
+		console.log(data)
+		weather.innerHTML() = `
+		<div>
+                <img src="https://openweathermap.org/img/wn/04n@2x.png" alt="Cloud Logo">
+            </div>
+            <div>
+                <h1>32 °C</h1>
+                <h4>Clear</h4>
+            </div>		
+		`
 }
 form.addEventListener(
 	"submit",
 	function(event) {
-		console.log(search.value)
+		getWeather(search.value)
 		event.preventDefault();
 	}
 )
