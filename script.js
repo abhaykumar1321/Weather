a1bb4b7e03681c6457c4b6ab76d1b1ea1b675661
abhaykumar1321 <abhaykumar132116@gmail.com>
2023-05-31 17:08:21 +0530

    weather api work

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const data = null;
+const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
+const form = document.querySelector("form")
+const search = document.querySelector("#search")
+const weather = document.querySelector("#weather")
+// const API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
 
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener('readystatechange', function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
+// const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+const getWeather = (city) => {
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+	const response = fetch(url);
+}
+form.addEventListener(
+	"submit",
+	function(event) {
+		console.log(search.value)
+		event.preventDefault();
 	}
-});
-
-xhr.open('GET', 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle');
-xhr.setRequestHeader('X-RapidAPI-Key', 'ed8716f0e9mshc8133f4a2d518f6p1ed515jsnc09ffa6124c7');
-xhr.setRequestHeader('X-RapidAPI-Host', 'weather-by-api-ninjas.p.rapidapi.com');
-
-xhr.send(data);
+)
